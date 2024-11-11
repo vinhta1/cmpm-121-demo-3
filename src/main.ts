0efbcb60a9fe5576636fc4166f66999560627a7f
Vinh Ta <tavinh132@gmail.com>
2024-11-11 12:02:50 -0800

    L16 Dialog

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
 // todo
+import "./style.css";
+
+const APP_NAME = "Eye Collector";
+document.title = APP_NAME;
+const appTitle = document.createElement("h1");
+appTitle.innerHTML = APP_NAME;
+
+const app = document.querySelector<HTMLDivElement>("#app")!;
+app.append(appTitle);
+
+const testButton = document.createElement("button");
+testButton.innerHTML = "Button";
+app.append(testButton);
+
+testButton.addEventListener("click", () => alert("You clicked the button!"));
